d95d4ae7f026483811b9b0f758f6b5a3ce4e7e3f
Muhammed Kilicarslan <mka281@icloud.com>
2020-08-03 09:25:37 -0400

    Add tests for message views

--- test_message_views.py
+++ test_message_views.py
@@ -1,9 +1,7 @@
 """Message View tests."""
-
 # run these tests like:
 #
-#    FLASK_ENV=production python -m unittest test_message_views.py
-
+# FLASK_ENV=production python -m unittest test_message_views.py
 
 import os
 from unittest import TestCase
@@ -14,22 +12,18 @@ from models import db, connect_db, Message, User
 # to use a different database for tests (we need to do this
 # before we import our app, since that will have already
 # connected to the database
-
 os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 
-
 # Now we can import app
-
 from app import app, CURR_USER_KEY
+
 
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
 # and create fresh new clean test data
-
 db.create_all()
 
 # Don't have WTForms use CSRF at all, since it's a pain to test
-
 app.config['WTF_CSRF_ENABLED'] = False
 
 
@@ -47,12 +41,12 @@ class MessageViewTestCase(TestCase):
         self.testuser = User.signup(username="testuser",
                                     email="test@test.com",
                                     password="testuser",
-                                    image_url=None)
+           s                         image_url=None)
 
         db.session.commit()
 
     def test_add_message(self):
-        """Can use add a message?"""
+        """Can user add a message?"""
 
         # Since we need to change the session to mimic logging in,
         # we need to use the changing-session trick:
@@ -63,7 +57,6 @@ class MessageViewTestCase(TestCase):
 
             # Now, that session setting is saved, so we can have
             # the rest of ours test
-
             resp = c.post("/messages/new", data={"text": "Hello"})
 
             # Make sure it redirects
@@ -71,3 +64,33 @@ class MessageViewTestCase(TestCase):
 
             msg = Message.query.one()
             self.assertEqual(msg.text, "Hello")
+
+    def test_show_message(self):
+        """Can user see the message?"""
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+
+            c.post(f"/messages/new", data={"text": "Hello"})
+            msg = Message.query.first()
+
+            resp = c.get(f"/messages/{msg.id}")
+            html = resp.get_data(as_text=True)
+            self.assertIn(msg.text, html)
+
+    def test_delete_message(self):
+        """Can user delete the message?"""
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess[CURR_USER_KEY] = self.testuser.id
+
+            c.post(f"/messages/new", data={"text": "Hello"})
+            msg = Message.query.first()
+            msg_id = msg.id
+            c.post(f"/messages/{msg_id}/delete")
+
+            msg = Message.query.get(msg_id)
+
+            self.assertIsNone(msg)
